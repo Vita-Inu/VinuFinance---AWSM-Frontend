437b752643012e20fe5916a38c864396ccedb68e
Robert <r.zavoronok@gmail.com>
2023-10-23 21:44:30 +0300

    Fix wagmi bug (#29)
    Co-authored-by: robert <robert@awsmagency.co>

--- src/features/wagmi/wagmi.tsx
+++ src/features/wagmi/wagmi.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useState } from 'react';
 import { configureChains, createConfig, WagmiConfig, mainnet } from 'wagmi';
 import { publicProvider } from 'wagmi/providers/public';
 
@@ -14,5 +14,14 @@ const config = createConfig({
 });
 
 export function Wagmi({ children }: PropsWithChildren) {
+  //NOTE: This is known Wagmi bug https://github.com/wagmi-dev/wagmi/issues/542
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return <WagmiConfig config={config}>{children}</WagmiConfig>;
 }
