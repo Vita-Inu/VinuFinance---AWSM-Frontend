ff3477cad4d3d9eabceec8c656a5dff2b5602dcf
Charterino <charterinod@gmail.com>
2023-11-02 13:26:28 +0300

    make the button display proper status and make it toggleable

--- src/features/borrowConfirm/components/button/Button.tsx
+++ src/features/borrowConfirm/components/button/Button.tsx
@@ -1,13 +1,33 @@
-import { Button as DefaultButton, BUTTON_PRESET } from '@/components/buttons';
+import {Button as DefaultButton, BUTTON_PRESET} from '@/components/buttons';
 
-import { Wrapper } from './styled';
+import {Wrapper} from './styled';
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
-export function Button() {
-  return (
-    <Wrapper>
-      <DefaultButton preset={BUTTON_PRESET.PINK} disabled={true}>
-        Send Borrow
-      </DefaultButton>
-    </Wrapper>
-  );
+type Props = {
+    borrowedAmnt: number;
+    borrowedToken: string;
+    collateralAmnt: number;
+    collateralToken: string;
+    enabled: boolean;
+    error: string;
+    onclick: VoidFunction;
+}
+
+export function Button(props: Props) {
+    return (
+        <Wrapper>
+            {!props.enabled &&
+                <DefaultButton preset={BUTTON_PRESET.PINK} disabled={true}>
+                    {props.error == undefined && 'Invalid parameters'}
+                    {props.error !== '' ? props.error : 'Invalid parameters'}
+                </DefaultButton>
+            }
+            {props.enabled &&
+                <DefaultButton onClick={props.onclick} preset={BUTTON_PRESET.WHITE}>
+                    Borrow {props.borrowedAmnt.toFixed(3)} {props.borrowedToken} for {props.collateralAmnt.toFixed(3)} {props.collateralToken}
+                </DefaultButton>
+            }
+        </Wrapper>
+    );
 }
