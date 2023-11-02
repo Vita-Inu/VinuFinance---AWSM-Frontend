9b9e161ed3e84e544463496cd3be7c6a20811870
Charterino <charterinod@gmail.com>
2023-11-02 13:26:44 +0300

    pass real data to BorrowConfirm and display it instead of default data

--- src/features/borrowConfirm/BorrowConfirm.tsx
+++ src/features/borrowConfirm/BorrowConfirm.tsx
@@ -1,63 +1,81 @@
-import { Box, Rows, Referral, Button, Row } from './components';
+import {Box, Rows, Referral, Button, Row} from './components';
+import {PoolAndSimulationResult} from "@/features/borrow";
+import {useEffect, useState} from "react";
+import {formatUnits} from "viem";
+import {Simulate} from "react-dom/test-utils";
+import input = Simulate.input;
 
-const FAKE_DATA = [
-  {
-    label: 'You Send',
-    value: '0 RPL',
-    explain: '($0.00)',
-    dateTime: null,
-  },
-  {
-    label: 'You Borrow',
-    value: '0 rETH',
-    explain: '($0.00)',
-    dateTime: null,
-  },
-  {
-    label: 'Repayment Amount',
-    value: '0 rETH',
-    explain: '($0.00)',
-    dateTime: null,
-  },
-  {
-    label: 'Due Amount',
-    value: null,
-    explain: null,
-    dateTime: {
-      date: 'November 5, 2023',
-      time: '12:50 PM',
-    },
-  },
-  {
-    label: 'Reclaimable Amount',
-    value: '0 RPL',
-    explain: '($0.00)',
-    dateTime: null,
-  },
-  {
-    label: 'Total Fees',
-    value: '0 RPL',
-    explain: '0 %',
-    dateTime: null,
-  },
-];
+type Props = {
+    inputAmnt: bigint;
+    pool: PoolAndSimulationResult;
+    onconfirmed: VoidFunction;
+}
 
-export function BorrowConfirm() {
-  return (
-    <Box>
-      <Rows>
-        {FAKE_DATA.map(({ explain, label, value, dateTime }, index) => (
-          <Row
-            key={index}
-            label={label}
-            value={value}
-            explain={explain}
-            dateTime={dateTime}
-          />
-        ))}
-      </Rows>
-      <Button />
-      <Referral />
-    </Box>
-  );
+export function BorrowConfirm({pool, inputAmnt, onconfirmed}: Props) {
+    let collateralAmnt = parseFloat(formatUnits(BigInt(inputAmnt.toString()), pool.collToken.decimals))
+    let borrowAmnt = parseFloat(formatUnits(BigInt(pool.loan[0].toString()), pool.loanToken.decimals))
+    let data = [
+        {
+            label: 'You Send',
+            value: collateralAmnt.toFixed(3) + ' ' + pool.collToken.symbol,
+            explain: '($0.00)',
+            dateTime: null,
+        },
+        {
+            label: 'You Borrow',
+            value: borrowAmnt.toFixed(3) + ' ' + pool.loanToken.symbol,
+            explain: '($0.00)',
+            dateTime: null,
+        },
+        {
+            label: 'Repayment Amount',
+            value: parseFloat(formatUnits(BigInt(pool.loan[1].toString()), pool.loanToken.decimals)).toFixed(3) + ' ' + pool.loanToken.symbol,
+            explain: '($0.00)',
+            dateTime: null,
+        },
+        {
+            label: 'Due Date',
+            value: '',
+            explain: '',
+            dateTime: new Date(Date.now() + parseInt(pool.pool.info[4].toString()) * 1000)
+        },
+        {
+            label: 'Reclaimable Amount',
+            value: parseFloat(formatUnits(BigInt(pool.loan[2].toString()), pool.collToken.decimals)).toFixed(3) + ' ' + pool.collToken.symbol,
+            explain: '($0.00)',
+            dateTime: null,
+        },
+        {
+            label: 'Total Fees',
+            value: parseFloat(formatUnits(BigInt(pool.loan[3].toString()), pool.collToken.decimals)).toFixed(3) + ' ' + pool.collToken.symbol,
+            explain: '0 %',
+            dateTime: null,
+        },
+    ]
+
+    return (
+        <Box>
+            <Rows>
+                {data.map(({explain, label, value, dateTime}, index) => (
+                    <Row
+                        key={index}
+                        label={label}
+                        value={value}
+                        explain={explain}
+                        dateTime={dateTime}
+                    />
+                ))}
+            </Rows>
+            <Button
+                enabled={pool.loan.error == ''}
+                borrowedAmnt={borrowAmnt}
+                collateralAmnt={collateralAmnt}
+                borrowedToken={pool.loanToken.symbol}
+                collateralToken={pool.collToken.symbol}
+                error={pool.loan.error}
+                onclick={onconfirmed}
+            />
+            <Referral/>
+        </Box>
+    );
 }
