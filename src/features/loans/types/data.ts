509603e6a11991061db2e499931e6f4708471c30
Charterino <charterinod@gmail.com>
2023-11-20 17:16:23 +0300

    update loan type

--- src/features/loans/types/data.ts
+++ src/features/loans/types/data.ts
@@ -1,12 +1,17 @@
-import { CURRENCY } from '@/utils/currency';
 import { ValueWithExplain } from '@/components/table';
 
 export type Loan = {
-  id: string;
-  loan: CURRENCY;
-  loanId: CURRENCY;
+  id: bigint;
+  uniqueId: string;
+  collateralTokenAddress: `0x{string}`;
+  collateralTokenName: string;
+  loanTokenAddress: `0x{string}`;
+  loanTokenName: string;
+  pool: `0x{string}`
   borrowed: ValueWithExplain;
   repayBefore: ValueWithExplain;
   repaymentAmount: ValueWithExplain;
+  repaymentAmountRaw?: bigint;
   collateralAmount: ValueWithExplain;
+  wasPaidOff: boolean; // if false it expired
 };
