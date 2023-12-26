979fa6f654d933592ddb2acf5c3637f0743f41d7
Robert <r.zavoronok@gmail.com>
2023-12-26 15:21:44 +0200

    Use borrow settings in transaction (#9)
    Co-authored-by: robert <robert@awsmagency.co>

--- src/features/borrowSettings/components/input/input.tsx
+++ src/features/borrowSettings/components/input/input.tsx
@@ -3,14 +3,16 @@ import { Value, Wrapper, Label, Field, Append } from './styled';
 type Props = {
   label: string;
   postfix: string;
+  onChange: (value: string) => void
+  value: string
 };
 
-export function Input({ label, postfix }: Props) {
+export function Input({ label, postfix, onChange, value }: Props) {
   return (
     <Wrapper>
       <Label>{label}</Label>
       <Value>
-        <Field />
+        <Field value={value} onChange={(evt) => onChange(evt.target.value)} />
         <Append>{postfix}</Append>
       </Value>
     </Wrapper>
