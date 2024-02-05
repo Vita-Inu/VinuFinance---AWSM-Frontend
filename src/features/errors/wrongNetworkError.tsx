305fd8fdb04f820a79a2ad9327ced01de5d064db
robert <robert@awsmagency.co>
2024-02-05 19:42:47 +0200

    Allow to switch network

--- src/features/errors/wrongNetworkError.tsx
+++ src/features/errors/wrongNetworkError.tsx
@@ -1,16 +1,21 @@
+import {useSwitchNetwork} from 'wagmi'
+
 import { Button, BUTTON_PRESET } from '@/components/buttons';
+import { vinuChain } from '@/const';
 
 import { Buttons, Container, Description, Title, Wrapper } from './components';
 
 export function WrongNetworkError() {
-  //TODO:: Adjust text
+
+  const { switchNetwork, isLoading } = useSwitchNetwork({chainId: vinuChain.id})
+
   return (
     <Wrapper>
       <Container>
         <Title>Wrong network selected</Title>
         <Description>Please connect to Vita Inu network</Description>
         <Buttons>
-          <Button preset={BUTTON_PRESET.WHITE}>Switch to Vita Inu</Button>
+          <Button preset={BUTTON_PRESET.WHITE} onClick={() => switchNetwork?.()} loading={isLoading}>Switch to Vita Inu</Button>
         </Buttons>
       </Container>
     </Wrapper>
